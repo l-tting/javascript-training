6f46acacd519febaa19165363c858e16df6d497f
LETTING <la@Lettings-MacBook-Pro.local>
2024-04-11 17:07:53 +0300

    bind

---
+++ binding.js
@@ -0,0 +1,10 @@
+// getting js data to html
+// let f_name= "Brian"
+// let s_name ="Letting"
+// let full = f_name +" " + s_name
+// document.getElementById("first").innerHTML = full
+
+//getting html to js
+
+let form = document.getElementById('myform')
+form.addEventListener('submit')
