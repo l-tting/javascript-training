2d5a56f93b840b105d69b05cedd5d092f92b5da0
LETTING <la@Lettings-MacBook-Pro.local>
2024-04-20 15:55:15 +0300

    m

--- salform.js
+++ salform.js
@@ -2,8 +2,14 @@ let form = document.getElementById('form')
 
 form.addEventListener('submit',function(e){
     e.preventDefault()
-    let basic =Number(document.getElementById('basic').value)
-    let benefits =Number(document.getElementById('benefits').value)
-    let gross = basic + benefits
-    document.getElementById('gross').innerHTML = `Gross salary is ${gross}`
-})
+    let basic =document.getElementById('basic').value
+    let benefits =document.getElementById('benefits').value
+    if(basic==""|| benefits==""){
+        document.getElementById('gross').innerHTML= "check input fields"
+    }
+    else{
+        let gross = Number(basic) + Number(benefits)
+        document.getElementById('gross').innerHTML = `Gross salary is ${gross}`
+}
+}) 
+    
